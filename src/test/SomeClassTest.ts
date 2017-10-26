82a10c89b4b3749472da6e8bdcc422f646757d28
okep <oldrich.kepka@gmail.com>
2017-10-26 20:50:35 +0200

    initial impl

---
+++ src/test/SomeClassTest.ts
@@ -0,0 +1,14 @@
+import * as assert from 'assert';
+
+import { SomeClass } from '../app/SomeClass';
+
+describe('Some class', () => {
+
+    let server;
+
+
+    it("can be constructed", () => {
+        const someClass = new SomeClass();
+    });
+
+});
